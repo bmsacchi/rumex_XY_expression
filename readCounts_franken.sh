7ea5ae4380a6d5b396ac0487bd2914af7e0f6fe3
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2024-03-14 12:05:38 -0400

    first commit

---
+++ readCounts_franken.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+featureCounts -v
+
+#featureCounts -p --countReadPairs -t exon -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX.txt RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
+
+#featureCounts -p --countReadPairs -t transcript -f -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_genes RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
+  
+featureCounts -p -t transcript -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_fragments RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
+
