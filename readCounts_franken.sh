5b167736fe994d601d6ac29c57a8740ea73e3b0d
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2024-03-19 11:34:45 -0400

    update readcounts

--- readCounts_franken.sh
+++ readCounts_franken.sh
@@ -4,7 +4,7 @@ featureCounts -v
 
 #featureCounts -p --countReadPairs -t exon -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX.txt RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
 
-#featureCounts -p --countReadPairs -t transcript -f -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_genes RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
+featureCounts -p --countReadPairs -t transcript -f -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_genes_noMulti RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.NoMulti.out.bam
   
-featureCounts -p -t transcript -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_fragments RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
+#featureCounts -p -t transcript -g gene_id -a genome/merged_TX_noMatPAR_main.gtf -o readCounts/countsFrankenTX_fragments RNAbams/merged_TX_noMatPAR/Rh_02_RNA_S122_L004_Aligned.sortedByCoord.out.bam
 
