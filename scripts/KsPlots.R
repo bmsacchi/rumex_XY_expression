d4084d9b412f851218ecb28c4c9fc84f02104e9d
Bianca Sacchi <43420897+bmsacchi@users.noreply.github.com>
2024-09-26 15:33:25 -0400

    new ds plots wiht updated hyphy ds estimates

--- scripts/KsPlots.R
+++ scripts/KsPlots.R
@@ -6,7 +6,7 @@ library(slider)
 ########################### raw data ###########################################
 ### in X-Y ortholog ASE data
 
-reads_pg<-read_csv("data/rna_ase_results_eqtl_sept12.csv.gz",show_col_types =FALSE)
+reads_pg<-read_csv("data/rna_ase_results_eqtl_sept12.csv.gz",show_col_types =FALSE) 
 
 ## read in ks dataframe 
 
@@ -21,11 +21,15 @@ hyphy_data<-read_csv("hyphy/all_ks_updated.csv.gz", show_col_types = FALSE) %>%
                               ifelse(grepl("TX_paternal", branch), "rhast_tx_pat", 
                                      ifelse(grepl("buc", branch), "bucephalophorus", 
                                             ifelse(grepl("Node2", branch), "node2", 
-                                                   ifelse(grepl("Node3", branch), "node3", NA)))))))
+                                                   ifelse(grepl("Node3", branch), "node3", NA))))))) 
 ### convert to wide format
+## and remove duplicate orthogroups randomly
 hyphy_wide<-hyphy_data %>% 
   pivot_wider(id_cols = OG, names_from =taxa, values_from = c("branch","dN","dS","LB","UB","MLE")) %>%
-  unite("pairID", c("branch_rhast_tx_mat", "branch_rhast_tx_pat"), sep = "_", remove = FALSE) 
+  unite("pairID", c("branch_rhast_tx_mat", "branch_rhast_tx_pat"), sep = "_", remove = FALSE) %>%
+  group_by(.,across(c(branch_node2, branch_sagittatus, pairID, branch_rhast_tx_mat, branch_rhast_tx_pat, branch_bucephalophorus,branch_node3))) %>%  
+  slice_sample(n = 1) %>% ungroup()
+
 
 ##################### Summarize dn and ds #################################
 hyphy_sum <- hyphy_wide %>% 
@@ -49,12 +53,12 @@ hyphy_reads_pg <- left_join(hyphy_sum, reads_pg) %>%
   mutate(yx_readRatio = male_mean.pat/male_mean.mat) %>%
   #mutate(yx_readRatio_plus = yx_readRatio+1) %>% 
   filter(yx_readRatio != 0) %>%
-  mutate(log2_readRatio=log2(yx_readRatio))
+  mutate(log2_readRatio=log2(yx_readRatio)) 
 
 ##Ks and gametolog expression plots 
 
 ## model
-ks_lm<-lm(formula = log2_readRatio ~ XYdS, data = dfKs)
+ks_lm<-lm(formula = log2_readRatio ~ XYdS, data = dfKs) 
 #plot(ks_lm)
 
 summary(ks_lm)
@@ -62,7 +66,9 @@ summary(ks_lm)
 
 #ggplot(dfKs) +geom_histogram(aes(log2_readRatio))
 
-## scatterplot of ds vs read ratio
+################################################################################
+################### ds expression scatterplots #################################
+
 ggplot(hyphy_reads_pg, aes(x = XYdS, y = log2_readRatio)) + 
   geom_point(alpha = 0.5) +
   #scale_y_continuous(limits=c(-10,10)) +
@@ -70,10 +76,10 @@ ggplot(hyphy_reads_pg, aes(x = XYdS, y = log2_readRatio)) +
   geom_smooth(method = "lm",formula=y~x) +
   #geom_abline(intercept = coef(ks_lm)[1],slope = coef(ks_lm)[2]) +
   ylab("log2 (y/x read ratio)") +
-  xlab("Ks") +
+  xlab("XYdS") +
   theme_bw(base_size = 20) +
   geom_hline(yintercept = 0, linetype = "dashed") 
-
+ggsave("figures/Ks_log2readRatio_scatter.png", h = 6, w = 8)
 
 ## but is it significant?
 lm_yx<-lm(formula = log2_readRatio ~ XYdS, data = hyphy_reads_pg)
@@ -81,6 +87,7 @@ lm_yx
 summary(lm_yx) # p value is 0.0001773
 # 
 
+###########################ks vs dn ############################################
 
 ### compare models
 
@@ -105,78 +112,125 @@ require(plyr)
 
 ggPredict(lm5,se=TRUE) + theme_bw() +  
   xlab("log2 (y/x read ratio)") +
-  ylab("difference in dN between Y and X gametologs") +
+  ylab("dN Y - dN X") +
   theme_bw(base_size = 18)
-#ggsave("figures/Figure2_dN_log2readRatio_ggPredict.png",h=6,w=8)
+ggsave("figures/Figure2_dN_log2readRatio_ggPredict.png",h=6,w=8)
 
 summary(lm5)# geom_text(x = -7, y = 0.25, label = lm_eqn(df), parse = TRUE)
 
-#### because I like to make life complicated: multilevel model?
+################### ks plot positions all genes ################################
 
+## merge annotation info with Ks data
 
-################################################################################
-################################################################################
-################### ks scatterplots ############################################
-ggplot(hyphy_reads_pg) + geom_point(aes(x=log2_readRatio, y=XYdS)) +
-  geom_smooth(aes(x=log2(yx_readRatio), y=XYdS), method = "lm") +
-  xlab("log2 (y/x read ratio)") +
-  ylab("Ks") +
+# load annotation
+anno<-vroom::vroom("data/merged_TX_noMatPARlarge_txanno.gtf",
+                   col_names=c("seqname","source","feature","start","end","score","strand","frame","attribute")) %>%
+  filter(feature =="transcript") %>%
+  separate(attribute, into =c("a","b"),sep = ";") %>% 
+  separate(a, into =c("a","Geneid"),sep = " ") %>% 
+  select(-c(a,b)) %>%
+  mutate(Geneid = str_remove_all(Geneid, "\"")) %>%
+  mutate(Geneid = str_remove_all(Geneid, "-RA"))
+
+# merge Ks data hwere available!
+hyphy_anno<- left_join(hyphy_sum, anno, by = c("branch_rhast_tx_pat" = "Geneid")) %>% 
+  left_join(.,anno, by = c("branch_rhast_tx_mat" = "Geneid"), suffix = c(".pat",".mat")) %>%
+  mutate(start.mat.mb = start.mat/1e6) %>%
+  mutate(start.pat.mb = start.pat/1e6) 
+
+# plot along Y position
+hyphy_anno %>% filter(seqname.pat == "Y") %>%
+  filter(XYdS <1) %>%
+  ggplot() + 
+  geom_point(aes(x=start.pat.mb, y=XYdS)) +
+  xlab("Y chromosome position (Mb)") +
+  geom_vline(xintercept = 45,linetype = "dashed", color = 'red') +
+  theme_bw(base_size = 20)
+  
+ggsave("figures/ds_Ypos.png", h = 6, w = 8)
+## plot along X position
+hyphy_anno %>% filter(seqname.mat == "X") %>%
+  filter(XYdS <1) %>%
+  ggplot() + 
+  geom_point(aes(x=start.mat.mb, y=XYdS)) +
+  xlab("X chromosome position (Mb)") +
+  theme_bw(base_size = 20) 
+ggsave("figures/ds_Xpos.png", h = 6, w = 8)
+
+## left_join with reads_pg
+pg_select<-reads_pg %>% select(1:27) %>% 
+  mutate(sigLFC = case_when(
+    res.allele.padj < 0.1 &
+      abs(res.allele.log2FoldChange) > 1 ~ "True",
+    TRUE ~"False" )) %>%
+  mutate(expType = case_when(sigLFC =="True" & res.allele.log2FoldChange <1 ~ "y_underexp",
+                             sigLFC =="True" & res.allele.log2FoldChange >1 ~ "y_overexp",
+                             sigLFC == "False" ~ NA))
+ # mutate(sigLFC = as.factor(sigLFC)) %>% 
+#  mutate(pairID = str_remove(pairID, "-RA"))
+
+hyphy_anno_reads <- hyphy_anno %>% left_join(.,pg_select, by = c("pairID")) %>% 
+  mutate(expType = ifelse(is.na(expType), NA, expType))
+
+### plot points along Y and color by type of expression
+
+hyphy_anno_reads %>% filter(seqname.pat == "Y") %>% filter(XYdS <0.5) %>%
+  ggplot() + 
+  geom_point(aes(x=start.pat.mb, y=XYdS, color = expType)) +
+  xlab("Y chromosome position (Mb)") +
+  geom_vline(xintercept = 45,linetype = "dashed", color = 'red') +
   theme_bw(base_size = 20) +
-  geom_vline(xintercept = 0, linetype = "dashed")
-#ggsave("figures/Ks_log2readRatio_scatter.png", h = 6, w = 8)
+  scale_color_manual(values=alpha(c("darkorange","darkblue"),c(0.75, 0.75)), 
+                     na.value = alpha("grey",0.3), labels = c("Y/X > 1", "Y/X < 1"))
+ggsave("figures/ds_Ypos_exp.png", h = 6, w = 8)
 
-#### ks vs dn
-ggplot(hyphy_reads_pg) + geom_point(aes(x=XYdS, y=YX_dNdiff)) + 
-  theme_bw(base_size = 20) +
-  xlab("dS") +
-  ylab("Y - X dN") +
-  geom_smooth(aes(x=XYdS, y=YX_dNdiff), method = "lm") 
+write_csv(hyphy_anno_reads, "data/hyphy_anno_reads.csv.gz")
 
+################# ks window plots ##############################################
 
-### ks across positions
-ggplot(hyphy_reads_pg) + geom_point(aes(x=log2_readRatio, y=XYdS)) +
-  geom_smooth(aes(x=log2(yx_readRatio), y=XYdS), method = "lm") +
-  xlab("log2 (y/x read ratio)") +
-  ylab("Ks") +
-  theme_bw(base_size = 20) +
-  geom_vline(xintercept = 0, linetype = "dashed")
-
-################# ks window plts ###############################################
-ks_mb<-hyphy_sum_match %>% mutate_at(c('start_tx_mat', 'start_tx_pat'), as.numeric) %>% 
-  mutate(start_tx_mat_mb = (start_tx_mat/1e6)) %>%
-  mutate(start_tx_pat_mb = (start_tx_pat/1e6))
 #write_csv(ks_mb, "data/ks_mb.csv.gz")
-windowmaker_1<- function(df,win,ks){df %>% filter(XYdS<ks) %>%
-    group_by(chr_tx_mat) %>%
-    arrange(id_tx_mat,chr_tx_mat, start_tx_mat) %>%
+windowmaker_mat<- function(df,win,ks){df %>% filter(XYdS<ks) %>%
+    group_by(seqname.mat) %>%
+    arrange(branch_rhast_tx_mat,seqname.mat, start.mat.mb) %>%
     mutate(Ks_win = slider::slide_dbl(XYdS, median, .before = win/2, .after = win/2,.step = 1,.complete =T))}
 
-windowmaker_1<- function(df,win,ks){df %>% filter(XYdS<ks) %>%
-    group_by(chr_tx_mat) %>%
-    arrange(id_tx_pat,chr_tx_pat, start_tx_mat) %>%
+windowmaker_pat<- function(df,win,ks){df %>% filter(XYdS<ks) %>%
+    group_by(seqname.pat) %>%
+    arrange(branch_rhast_tx_pat,seqname.pat, start.pat.mb) %>%
     mutate(Ks_win = slider::slide_dbl(XYdS, median, .before = win/2, .after = win/2,.step = 1,.complete =T))}
 
-ks_win100<-windowmaker_1(ks_mb,win = 100,1)
-# ks_win100_x<-windowmaker_2(ks_mb, win = 100, 0.3)
+ks_win100<-windowmaker_pat(hyphy_anno,win = 100,0.5) %>% drop_na(seqname.pat)
+#ks_win50<-windowmaker_pat(hyphy_anno,win = 50,0.5)
 
-ks_win50<-windowmaker_1(ks_mb,win = 50,1)
-#ks_win20_x<-windowmaker_2(ks_mb, win = 20, 0.3)
-#library(RColorBrewer)
-ks_win30<-windowmaker_1(ks_mb,win = 30,1)
 
-ggplot(ks_win100, aes(start_tx_pat_mb,group = chr_tx_mat)) +
-  geom_line(aes(y=Ks_win, colour = chr_tx_mat)) +
-  #facet_grid(~chr_tx_mat,scales="free_x") +
-  #ylim(0,0.1) +
+ggplot(ks_win100, aes(start.pat.mb,group = seqname.pat)) +
+  geom_line(aes(y=Ks_win, colour = seqname.pat)) +
+  facet_grid(~seqname.pat,scales="free_x") +
+  #ylim(0,0.1) + 
   scale_color_brewer(palette = "Dark2") +
-  xlab("X-bearing Haplotype\nposition(Mb)") +
+  xlab("Y-bearing Haplotype\nposition(Mb)") +
   ylab("Median Ks\n(100 gene windows)") +
   theme_classic()
 
-ggsave("figures/Ks_coords_allchr_TX_mat_100win.jpg", h = 5, w = 9)
+ggsave("figures/ds_allchr_pat_100win.png", h = 5, w = 9)
+
+## y plot window only
+ks_win100 %>% filter(seqname.pat == "Y") %>% 
+  ggplot(aes(start.pat.mb,group = seqname.pat)) +
+  geom_line(aes(y=Ks_win, colour = seqname.pat)) +
+  facet_grid(~seqname.pat,scales="free_x") +
+  #ylim(0,0.1) + 
+  scale_color_brewer(palette = "Dark2") +
+  xlab("Y chromosome position(Mb)") +
+  ylab("Median Ks\n(100 gene windows)") +
+  geom_vline(xintercept = 45,linetype = "dashed", color = 'red') +
+  theme_classic()
+ggsave("figures/ds_Ychr_pat_100win.png", h = 5, w = 9)
+
 
 ### compare with "badgenes" 
 # badgenes<-read_csv("data/badgenes.csv")
+
 rnaCountsClean<-read_csv("data/rnaCountsClean.csv.gz")
 badgenes<-rnaCountsClean %>% 
   filter(female_mean>= 20 & Chr =="Y") %>% select(Geneid)
@@ -201,25 +255,21 @@ ggplot(hyphy_badgenes) + geom_density(aes(x=XYdS, fill = filterType), alpha = 0.
   xlab("Ks") +
   ylab("Density") +
   theme_bw(base_size = 20) +
-  xlim(0,0.3)
-  scale_fill_manual(values = c("goodgene" = "blue", "badgene" = "red"))
+  xlim(0,0.3) #+
+#  scale_fill_manual(values = c("goodgene" = "blue", "badgene" = "red"))
+ggsave("figures/mismapping_kS_density.png", h = 6, w = 8)
 
-## boxplot
- 
-hyphy_badgenes%>% filter(XYdS <0.2) %>% ggplot() + geom_boxplot(aes(x=filterType, y=(XYdS), fill = filterType)) +
-  xlab("Gene type") +
-  ylab("Ks") +
-  theme_bw(base_size = 20) +
-  scale_fill_manual(values = c("goodgene" = "blue", "badgene" = "red"))
-### sample part of the data
+## histogram
 
-hyphy_badgenes %>% group_by(filterType) %>% slice_sample(n = 100) %>%
-  filter(XYdS <0.2) %>%
-  ggplot() + geom_boxplot(aes(x=filterType, y=(XYdS), fill = filterType)) +
-  xlab("Gene type") +
-  ylab("Ks") +
+ggplot(hyphy_badgenes) + 
+  geom_histogram(aes(x=XYdS, fill = filterType), alpha = 0.5) +
+  xlab("Ks") +
+  ylab("Density") +
   theme_bw(base_size = 20) +
+  xlim(0,0.3) +
   scale_fill_manual(values = c("goodgene" = "blue", "badgene" = "red"))
+ggsave("figures/mismapping_kS_histogram.png", h = 6, w = 8)
+
 ## no apparent difference
 ## odd!
 rnaCountsClean<-read_csv("data/rnaCountsClean.csv.gz") %>% select(Geneid,Chr,Start,female_mean)
--- scripts/changePointKs.R
+++ scripts/changePointKs.R
@@ -1,15 +1,25 @@
 library(tidyverse)
 library(mcp)
+library(rjags)
 ########################### Ks step change ##################################
-ks_mb<-read_csv("data/ks_mb.csv.gz")
+hyphy_anno_reads <- read_csv("data/hyphy_anno_reads.csv.gz")
 
-ks_mb %>% ggplot() +geom_point(aes(x = start_tx_mat_mb, y = XYdS)) +
+hyphy_anno_reads %>% filter(seqname.pat =="Y" & XYdS <0.5) %>% 
+  ggplot() + geom_point(aes(x = start.pat.mb, y = XYdS)) +
 #  geom_step(aes(x = start_tx_mat_mb, y = XYdS)) +
   xlab("X-bearing Haplotype\nposition(Mb)") +
   ylab("Ks") +
   theme_classic()
-ks_mb %>% ggplot() +geom_point(aes(x = start_tx_pat_mb, y = XYdS)) +
-  #  geom_step(aes(x = start_tx_mat_mb, y = XYdS)) +
-  xlab("X-bearing Haplotype\nposition(Mb)") +
-  ylab("Ks") +
-  theme_classic()
+
+## define the model
+model = list(
+  response ~ 1,  # plateau (int_1)
+  ~ 0 + time,    # joined slope (time_2) at cp_1
+  ~ 1 + time     # disjoined slope (int_3, time_3) at cp_2
+)
+#get data and fit it
+ex = mcp_example("demo")
+fit = mcp(model, ex$data)
+## run this on a server my god!
+plot(fit)
+
